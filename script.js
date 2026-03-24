4ba9e45875e7774f80fa47949108b5f17cf36ae0
Asingile-CARS <asingileallen@gmail.com>
2026-03-24 16:44:45 +0300

    Added car image slideshow

---
+++ script.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll('.carousel-image');
+let current = 0;
+
+function showNextImage() {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}
+
+// start first image
+images[current].classList.add('active');
+
+// change image every 3 seconds
+setInterval(showNextImage, 3000);
