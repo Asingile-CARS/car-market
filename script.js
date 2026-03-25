18eb9dc3098fa5e3194989a26983050a60ecee44
Asingile-CARS <asingileallen@gmail.com>
2026-03-25 10:40:07 +0300

    Full carousel with arrows and dots

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const images = document.querySelectorAll('.carousel-image');
+const dots = document.querySelectorAll('.dot');
+
 let current = 0;
 
-function showNextImage() {
-  images[current].classList.remove('active');
-  current = (current + 1) % images.length;
-  images[current].classList.add('active');
+function showImage(index) {
+  images.forEach(img => img.classList.remove('active'));
+  dots.forEach(dot => dot.classList.remove('active'));
+
+  images[index].classList.add('active');
+  dots[index].classList.add('active');
 }
 
-// start first image
-images[current].classList.add('active');
+function nextImage() {
+  current = (current + 1) % images.length;
+  showImage(current);
+}
 
-// change image every 3 seconds
-setInterval(showNextImage, 7000);
+function prevImage() {
+  current = (current - 1 + images.length) % images.length;
+  showImage(current);
+}
+
+function setImage(index) {
+  current = index;
+  showImage(current);
+}
+
+// start
+showImage(current);
+
+// auto slide
+setInterval(nextImage, 3000);
+
+
 
 function filterCars() {
     const input = document.getElementById('searchInput').value.toLowerCase();
